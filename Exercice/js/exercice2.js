3c775835275e2a0c9803efbd2ee1f0b4868ae285
Wassila <wassilaboukedroun@outlook.fr>
2023-05-25 13:43:34 +0200

    js exercice

---
+++ Exercice/js/exercice2.js
@@ -0,0 +1,11 @@
+
+var nb1 = prompt();
+console.log(typeof nb1);
+nb1 = parseInt(nb1);
+console.log(typeof nb1);
+var nb2 =7;
+resultat = nb1 % nb2;
+console.log(resultat);
+document.write(resultat);
+window.alert(resultat);
+resultat += nb2;
