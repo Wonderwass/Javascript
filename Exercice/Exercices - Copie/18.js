3b75d27d1fbe4295efda9f2a794dbdc9fa15e490
Wassila <wassilaboukedroun@outlook.fr>
2023-06-26 16:57:42 +0200

    n

--- Exercice/Exercices - Copie/18.js
+++ Exercice/Exercices - Copie/18.js
@@ -1,7 +1,17 @@
-// Solution utilisant une série de if/else
-for (var nombre = 1; nombre <= 100; nombre++) {
-    if ((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
-    else if (nombre % 3 === 0) console.log("Fizz");
-    else if (nombre % 5 === 0) console.log("Buzz");
-    else console.log(nombre);
+for (nombre = 1; nombre <= 100; nombre++){
+    if ((nombre % 3 == 0) && (nombre % 5 == 0)) {
+        document.write("FizzBuzz <br>");
+    
+    }
+    else if (nombre % 5 == 0) {
+        document.write("Buzz <br>");
+    }
+    else if (nombre%3==0){
+    document.write("Fizz<br>");
+    }
+    else {
+        document.write(nombre+"<br>");
+    }
+    
+    
 }
